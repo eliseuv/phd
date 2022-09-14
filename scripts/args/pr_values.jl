055734addaf2694bd2e993e2d526ae6dd2d0c5aa
evf <eliseuv816@gmail.com>
2022-09-14 20:17:28 -0300

    update

--- scripts/args/pr_values.jl
+++ scripts/args/pr_values.jl
@@ -1,4 +1,4 @@
-for (r, p, L) in Iterators.product(range(0, 1, 101), 0.3, 128)
+for (p, L) in Iterators.product(collect(0.0:0.1:1.0), 128)
     #r = 0.194421 + dr
-    println("$L $p $r")
+    println("$L $p")
 end
