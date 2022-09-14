4e399d40ca94fe5f95a74b97a7e48b4a9e5e8c5d
evf <eliseuv816@gmail.com>
2022-09-14 13:47:16 -0300

    Lovelace update

--- scripts/args/pr_values.jl
+++ scripts/args/pr_values.jl
@@ -1,4 +1,4 @@
-for (dr, p, L) in Iterators.product(range(-0.1, 0.1, 11), 0.3, [16, 32, 64, 128, 256])
-    r = 0.194421 + dr
+for (r, p, L) in Iterators.product(range(0, 1, 101), 0.3, 128)
+    #r = 0.194421 + dr
     println("$L $p $r")
 end
