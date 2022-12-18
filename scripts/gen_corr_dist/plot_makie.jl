06aa469e9e2ac8d67833468edfa518622b6a7b7a
evf <eliseuv816@gmail.com>
2022-12-18 01:38:23 -0300

    update

--- scripts/gen_corr_dist/plot_makie.jl
+++ scripts/gen_corr_dist/plot_makie.jl
@@ -3,7 +3,7 @@ using DrWatson
 @quickactivate "phd"
 
 # External libraries
-using Logging, JLD2, DataFrames, CairoMakie
+using Logging, JLD2, DataFrames, CairoMakie, ColorSchemes
 
 # Custom modules
 include("../../src/Thesis.jl")
@@ -16,7 +16,7 @@ const params_req = Dict("gamma" => 1, "dist" => "Distances.euclidean")
 const measure = "costs"
 
 # Color key
-const color_key = "sigma"
+const key_name = "sigma"
 
 # Find datafiles
 datafiles =
@@ -26,25 +26,40 @@ datafiles =
         params_req,
         ext="jld2")
 
-fig = Figure()
-ax = Axis(fig[1, 1])
+key_values = Set{Float64}()
+for datafile ∈ datafiles
+    push!(key_values, datafile.params[key_name])
+end
+key_extrema = extrema(key_values)
+@inline normalize(x) = (x - key_extrema[1]) / (key_extrema[2] - key_extrema[1])
 
-key_values = Vector()
+@show key_values
+
+fig = Figure(resolution=(1024, 768))
+ax = Axis(fig[1, 1],
+    title="Generate Uniform Correlation Distribution using Simulated Annealing " * join([name * " = " * string(value) for (name, value) ∈ params_req], ", "),
+    xlabel="iter",
+    ylabel="Normalied cost")
+
 for datafile ∈ datafiles
 
     @show datafile
 
-    key_value = datafile.params[color_key]
-    push!(key_values, key_value)
+    key_value = datafile.params[key_name]
 
     df = load(datafile.path, "df")
     df[!, "norm_"*measure] = df[!, measure] ./ df[!, measure][begin]
 
-    lines!(ax, df[!, "norm_"*measure],)
+    lines!(ax, df[!, "norm_"*measure],
+        label=L"%$(key_value)",
+        color=get(ColorSchemes.viridis, normalize(key_value)))
 end
+
+# Add legend
+Legend(fig[1, 2], ax, label=L"\sigma")
 
 @inline params_str(params::Dict{String,T}) where {T} = join([string(name) * " = " * string(value) for (name, value) ∈ params], ", ")
 
-const output_plotname = plotsdir(filename("GenUniformCorrDistSA_norm_" * measure, params_req, "var" => color_key, ext="png"))
+const output_plotname = plotsdir(filename("GenUniformCorrDistSA_norm_" * measure, params_req, "var" => key_name, ext="png"))
 @show output_plotname
 save(output_plotname, fig)
