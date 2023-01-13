8549ac33fec7512d1afc8197d9c2406a3c2c7b8f
evf <eliseuv816@gmail.com>
2023-01-13 12:19:48 -0300

    update

--- scripts/args/distances.jl
+++ scripts/args/distances.jl
@@ -16,6 +16,6 @@ distances_list = ["Distances.euclidean",
     "Distances.msd",
     "Distances.rmsd",
     "Distances.nrmsd"]
-for (sigma, dist_str) in Iterators.product(0.001:0.001:0.01, ["Distances.sqeuclidean"])
+for (sigma, dist_str) in Iterators.product(0.001:0.001:0.1, ["Distances.sqeuclidean"])
     println("$sigma $dist_str")
 end
