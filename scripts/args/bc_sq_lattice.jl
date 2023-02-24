d943d461a7d58cb13dcca8917f958fb2b227e4e2
evf <eliseuv816@gmail.com>
2023-02-24 20:06:26 -0300

    update

--- scripts/args/bc_sq_lattice.jl
+++ scripts/args/bc_sq_lattice.jl
@@ -2,6 +2,6 @@ const D = 1.5
 const β_c = 1.0 / 1.3986
 const τ_vals = 2.0 .^ (-0.2:0.05:0.2)
 
-for (beta, L) ∈ Iterators.product(β_c .* τ_vals, 2 .^ (7:7))
+for (beta, L) ∈ Iterators.product(β_c .* τ_vals, 2 .^ (6:6))
     println("$L $D $beta")
 end
